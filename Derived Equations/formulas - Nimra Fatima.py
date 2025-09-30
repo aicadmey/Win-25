1e12e0aeac31d8c3a688ddf4040f9e55af3847e2
AI Cadmey <140780117+aicadmey@users.noreply.github.com>
2025-09-30 04:30:25 -0700

    Add files via upload

---
+++ Derived Equations/formulas - Nimra Fatima.py
@@ -0,0 +1,24 @@
+import math
+F = 3
+m = 2
+b = 2
+N = m * b
+X_ = N * F
+s = 5
+u = 5
+i = s + u
+e = 3
+g = 2
+w = e + g
+r = i-w
+h = 2
+p = r*h
+t = 3
+v = 3
+a = v + t
+X= p - a
+j = 2
+k = 2
+n= j + k
+y = (X-X_)**2/n
+print(y)
